077d30967fc29749f0d0f4d8375a347144de29a6
Janoš Vidali <janos.vidali@fri.uni-lj.si>
2017-06-09 13:04:52 +0200

    Add link and update cronjob

--- cronjob.sh
+++ cronjob.sh
@@ -4,4 +4,9 @@ wget -N -r --no-parent http://www.win.tue.nl/~aeb/graphs/
 wget -N -r --no-parent http://www.win.tue.nl/~aeb/drg/drgtables.html
 git add www.win.tue.nl
 git commit -m "Update $(date "+%Y-%m-%d")" --author "Andries E. Brouwer <aeb@cwi.nl>"
+
+wget -N -r --no-parent http://www.uwyo.edu/jwilliford/data/qprim3_table.html
+git add www.uwyo.edu
+git commit -m "Update $(date "+%Y-%m-%d")" --author "Jason S. Williford <jwillif1@uwyo.edu>"
+
 git push
